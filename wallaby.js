67f97cf745090a2bd64eaf3ee7072fd589beed49
Azariah Engelbrecht <azaeng04@gmail.com>
2024-11-07 13:12:08 +0200

    update packages

--- wallaby.js
+++ wallaby.js
@@ -1,9 +1,4 @@
 
 module.exports = () => ({
-  autoDetect: false,
-  files: ['vite.config.mts', 'src/**/*.ts', '!src/**/*.spec.ts'],
-  tests: ['src/**/*.spec.ts'],
-  testFramework: {
-    configFile: './vite.config.mts'
-  }
+  autoDetect: true
 });
